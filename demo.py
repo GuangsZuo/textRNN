a41af17ccf0d61792bb5029f1be503470ae8fbde
poem <qf.zgs.1994@gmail.com>
2018-05-25 16:38:07 +0800

    demo fix

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 from algo import textrnn_algo
 from text_processing import *
-
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 embeding_file="./glove.840B.300d.txt"
 embed_size = 300
 max_words = 20000
@@ -9,8 +10,8 @@ sentence_size = 30
 model_file = "./best_model.hdf5"
 
 if __name__ == "__main__":
-    neg = pd.read_csv("./MR/mr.neg", delimiter="\n", encoding="ISO-8859-1", header=None)
-    pos = pd.read_csv("./MR/mr.pos", delimiter="\n", encoding="ISO-8859-1", header=None)
+    neg = pd.read_csv("./data/MR/mr.neg", delimiter="\n", encoding="ISO-8859-1", header=None)
+    pos = pd.read_csv("./data/MR/mr.pos", delimiter="\n", encoding="ISO-8859-1", header=None)
 
     neg = neg.rename(index=str, columns={0: "text"})
     pos = pos.rename(index=str, columns={0: "text"})
@@ -26,11 +27,12 @@ if __name__ == "__main__":
 
     word_embeding,max_words = get_word_embding(word_index, embeding_file, max_words, sentence_size)
 
-    algo = textrnn_algo(train_data, word_embeding, model_file, classes=2, sentence_length=sentence_size,
+    algo = textrnn_algo(train_data, word_embeding, model_file, classes=1, sentence_length=sentence_size,
                         embed_size=embed_size, target_is_prob=False, word_size=max_words,
                         is_sigmoid_loss=True, learning_rate=1e-3, epoch=100, early_stopping_rounds=3,
                         batch_size=64)
     algo.model_train()
-    algo.model_predict()
+    result = algo.model_predict()
+    print(result)
 
 
