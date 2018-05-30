f5bb778d9204558e6269ea0617dec195adf735b6
zuogs <qfzgs1994@163.com>
2018-05-30 17:07:50 +0800

    worked, not tuning.

--- algo.py
+++ algo.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+from tqdm import tqdm
 
 from model import textrnn
 
@@ -34,31 +35,35 @@ class textrnn_algo:
     def model_train(self):
         x_train, y_train, x_val, y_val = self.split_train_data(self.data)
         self.sess.run(tf.global_variables_initializer()) #TODO: move this to model
+        self.best_score = 2147483644
+        self.bad_rounds = 0
         for epoch in range(1, self.epoch+1):
-            best_score = 2147483644
-            bad_rounds = 0
             train_loss = 0
-            for st,ed in zip(range(0,x_train.shape[0], self.batch_size), range(self.batch_size, x_train.shape[0]+self.batch_size, self.batch_size)):
+            counter = 0 
+            for st,ed in tqdm(zip(range(0,x_train.shape[0], self.batch_size), range(self.batch_size, x_train.shape[0]+self.batch_size, self.batch_size))):
+                if ed > x_train.shape[0]: ed=x_train.shape[0]
                 x_train_batch = x_train[st:ed]
                 y_train_batch = y_train[st:ed]
-                losses, _ = self.sess.run([self.model.losses, self.model.trainop],
+                losses, _, summary, step = self.sess.run([self.model.losses, self.model.trainop, self.model.merged, self.model.global_step],
                               feed_dict={self.model.input: x_train_batch, self.model.target: y_train_batch})
                 train_loss += np.sum(losses)
+                counter += 1
+                self.writer.add_summary(summary, step)
             train_loss = train_loss / x_train.shape[0] / self.classes
             val_loss = self.evaluate_model(x_val, y_val)
             print("Epoch %d: train loss : %.6f, val loss: %.6f" % (epoch, train_loss, val_loss))
-            if val_loss < best_score:
+            if val_loss <= self.best_score:
                 print("*** New best score ***\n")
-                best_score = val_loss
-                bad_rounds = 0
+                self.best_score = val_loss
+                self.bad_rounds = 0
                 self.saver.save(self.sess, self.model_file)
             else:
-                bad_rounds += 1
-                if bad_rounds >= self.early_stopping_rounds:
-                    print("Epoch %05d: early stopping, best score = %.6f" % (epoch, best_score))
+                self.bad_rounds += 1
+                if self.bad_rounds >= self.early_stopping_rounds:
+                    print("Epoch %05d: early stopping, best score = %.6f" % (epoch, self.best_score))
                     break
 
-    def evaluate_model(self, x_val, y_val, batch_size=1024):
+    def evaluate_model(self, x_val, y_val, batch_size=128):
         val_loss = 0
         for st, ed in zip(range(0, x_val.shape[0], batch_size),
                           range(batch_size, x_val.shape[0] + batch_size, batch_size)):
@@ -81,9 +86,11 @@ class textrnn_algo:
         result = np.zeros((test_data.shape[0],self.classes))
         for st, ed in zip(range(0, test_data.shape[0], batch_size),
                           range(batch_size, test_data.shape[0] + batch_size, batch_size)):
-            x_val_batch = test_data["text"][st:ed]
-            output = self.sess.run([self.model.output], feed_dict={self.model.input: x_val_batch})
-            result[st:ed] = output
+            if ed>test_data.shape[0]: ed=test_data.shape[0]
+            x_val_batch = test_data[st:ed]
+            output = self.sess.run([self.model.output,], feed_dict={self.model.input: x_val_batch})
+            print(type(output[0]))
+            result[st:ed] = output[0]
         return result
 
 
--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from tensorflow.contrib import rnn
 
 class textrnn:
     def __init__(self, classes, sentence_length, embed_size, target_is_prob, word_size,
-                 is_sigmoid_loss=True, learning_rate=1e-3, optimizer=tf.train.AdamOptimizer, word_embeding=None, **kwargs):
+                 is_sigmoid_loss=True, learning_rate=1e-3, optimizer=tf.train.AdamOptimizer, word_embeding=None,batch_size=64, **kwargs):
         '''
         :param classes: class labels count.
         :param sentence_length
@@ -16,12 +16,14 @@ class textrnn:
         '''
         self.word_size = word_size
         self.embed_size = embed_size
+        self.batch_size = batch_size
         self.input = tf.placeholder(tf.int32, shape=(None, sentence_length)) # feed word_index sequence
         self.target = tf.placeholder(tf.float32, shape=(None, classes))
         self.sentence_size = sentence_length
         self.classes = classes
         self.is_sigmoid_loss = is_sigmoid_loss
         self.learning_rate = tf.Variable(learning_rate, trainable=False, dtype=tf.float32, name="learning_rate")
+        self.global_step = tf.get_variable("global_step", shape=(),dtype=tf.int32, trainable=False, initializer=tf.constant_initializer(1))
         self.optimizer = optimizer(self.learning_rate)
         if word_embeding is None:
             raise Exception("word embeding could not be None!")
@@ -54,6 +56,7 @@ class textrnn:
         self.define_graph()
         self.define_loss()
         self.define_optimizer()
+        self.merged = tf.summary.merge_all()
 
     def init_weight(self):
         with tf.variable_scope("init_weight"):
@@ -62,55 +65,50 @@ class textrnn:
     def load_embeding(self, embeding):
         with tf.variable_scope("load_embeding"):
             self.word_embeding = tf.get_variable("word_embeding",shape=(self.word_size, self.embed_size),
-                                            dtype=tf.float32, initializer=tf.constant_initializer(embeding))
+                                            dtype=tf.float32, initializer=tf.constant_initializer(embeding), trainable=False)
 
     def lstm(self):
-        with tf.variable_scope("lstm_Cell",reuse=tf.AUTO_REUSE):
+        with tf.variable_scope("lstm_Cell"):
             lstm_cell = rnn.LSTMCell(self.rnn_units, reuse=tf.get_variable_scope().reuse)
         return lstm_cell
 
     def define_graph(self):
         with tf.variable_scope("embeding_lookup"):
             features = tf.nn.embedding_lookup(self.word_embeding, self.input)
-        with tf.variable_scope("bilstm_layer_1"):
-            cell_fw = [self.lstm() for i in range(self.sentence_size)]
-            cell_bw = [self.lstm() for i in range(self.sentence_size)]
-            h_states,_,_ = rnn.stack_bidirectional_dynamic_rnn(cell_fw, cell_bw, features, dtype=tf.float32)
-        with tf.variable_scope("bilstm_layer_2"):
-            cell_fw = [self.lstm() for i in range(self.sentence_size)]
-            cell_bw = [self.lstm() for i in range(self.sentence_size)]
-            h_states, _, _ = rnn.stack_bidirectional_dynamic_rnn(cell_fw, cell_bw, h_states, dtype=tf.float32) # (batch_size, sentence_size, num_units*2)
+        with tf.variable_scope("bilstm_layer"):
+            cell_fw = self.lstm()
+            cell_bw = self.lstm()
+            outputs,_ = tf.nn.bidirectional_dynamic_rnn(cell_fw,cell_bw,features,dtype=tf.float32)
+            outputs = tf.concat(outputs, axis=2)
         with tf.variable_scope("pooling_layer"):
-            maxpool_layer = tf.layers.MaxPooling1D(pool_size=self.sentence_size, strides=1, padding="valid")(h_states)
-            avepool_layer = tf.layers.AveragePooling1D(pool_size=self.sentence_size, strides=1, padding="valid")(h_states)
-            # (batch_size, num_units*2)
-            print(maxpool_layer.shape, avepool_layer.shape)
+            maxpool_layer = tf.layers.MaxPooling1D(pool_size=self.sentence_size, strides=1, padding="valid")(outputs)
+            avepool_layer = tf.layers.AveragePooling1D(pool_size=self.sentence_size, strides=1, padding="valid")(outputs)
         with tf.variable_scope("concat_layer"):
             concat_layer = tf.concat([maxpool_layer, avepool_layer], axis=1)
             concat_layer = tf.reshape(concat_layer, [-1, self.rnn_units*4])
-            print(concat_layer.shape)
         with tf.variable_scope("fc_layer"):
             fc_layer = tf.layers.dense(concat_layer, self.fc_units, kernel_initializer=self.fc_kinit,
                                         activation=tf.nn.relu)
-            self.logits = tf.layers.dense(fc_layer, self.classes, kernel_initializer=self.fc_kinit,
-                                         activation=None)
+            self.logits = tf.layers.dense(fc_layer, self.classes, kernel_initializer=tf.truncated_normal_initializer(stddev=0.1),activation=None)
         with tf.variable_scope("output_layer"):
             self.output = tf.sigmoid(self.logits) #TODO
 
-    def define_loss(self):
+    def define_loss(self): #TODO
         if self.is_sigmoid_loss:
             with tf.variable_scope("compute_loss"):
                 self.losses = tf.nn.sigmoid_cross_entropy_with_logits(labels=self.target, logits=self.logits)
-                self.loss = tf.reduce_mean(self.losses)  + \
-                            tf.add_n([tf.nn.l2_loss(v)  for v in tf.trainable_variables() if "bias" not in v.name]) * self.l2_lambda
+                self.loss = tf.reduce_mean(self.losses) # + \
+                            #tf.add_n([tf.nn.l2_loss(v)  for v in tf.trainable_variables() if "bias" not in v.name]) * self.l2_lambda
         else:
             with tf.variable_scope("compute_loss"):
                 self.losses = tf.nn.softmax_cross_entropy_with_logits(labels=self.target, logits=self.logits)
-                self.loss = tf.reduce_mean(self.losses) + \
-                            tf.add_n([tf.nn.l2_loss(v)  for v in tf.trainable_variables() if "bias" not in v.name]) * self.l2_lambda
+                self.loss = tf.reduce_mean(self.losses) #+ \
+                            #tf.add_n([tf.nn.l2_loss(v)  for v in tf.trainable_variables() if "bias" not in v.name]) * self.l2_lambda
 
     def define_optimizer(self):
         with tf.variable_scope("optimize"):
-            train_vars = tf.trainable_variables()
-            grads_vars = self.optimizer.compute_gradients(self.loss, var_list=train_vars)
-            self.trainop = self.optimizer.apply_gradients(grads_vars)
+            self.trainop = tf.contrib.layers.optimize_loss(self.loss, self.global_step, None, self.optimizer, summaries=["gradients","loss"])
+#             train_vars = tf.trainable_variables()
+#             grads_vars = self.optimizer.compute_gradients(self.loss, var_list=train_vars)
+#             self.trainop = self.optimizer.apply_gradients(grads_vars)
+       
